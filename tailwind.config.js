bc76e6c0b51ce163a4a12292ae4fc24869ed95f2
aleKlOl <105486891+aleKlOl@users.noreply.github.com>
2024-04-29 09:18:58 +0500

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './dikamShop/templates/dikamShop/**/*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
